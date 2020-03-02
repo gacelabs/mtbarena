5c2aefd8fa05dda1c7413018b7073462d95477b5
Eddie <eddie@ypdigital.com.au>
2020-03-02 21:31:22 +0800

    final integrations

--- assets/js/defaults.js
+++ assets/js/defaults.js
@@ -220,19 +220,19 @@ $(function() {
 });
 
 function popupCenter(url, oThis, title, id, w, h) {
-	/*if (w == undefined) w = 450;
+	if (w == undefined) w = 450;
 	if (h == undefined) h = 450;
 	var left = (screen.width/2)-(w/2);
 	var top = (screen.height/2)-(h/2);
 	var win = window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 	// console.log(oThis, win);
-	return win;*/
-	FB.ui({
+	return win;
+	/*FB.ui({
 		method: 'share',
 		href: url
 	}, function(response){
 		console.log(response);
-	});
+	});*/
 }
 
 var recentAjax = false;
