144a36496b66cf17545aec643743d85d4a18036f
Eddie <eddie@ypdigital.com.au>
2020-03-16 21:08:55 +0800

    updates in match up page

--- application/views/dashboard_elements/menu.php
+++ application/views/dashboard_elements/menu.php
@@ -15,9 +15,11 @@
 				<a href="dashboard/profile">
 					<p class="<?php echo ($body_class == "profile" ? 'active' : ''); ?>">Profile</p>
 				</a>
-				<a href="dashboard/settings">
-					<p class="<?php echo ($body_class == "settings" ? 'active' : ''); ?>">Settings</p>
-				</a>
+				<?php if ($current_profile['is_admin']): ?>
+					<a href="dashboard/settings">
+						<p class="<?php echo ($body_class == "settings" ? 'active' : ''); ?>">Settings</p>
+					</a>
+				<?php endif ?>
 			</div>
 		</div>
 	</div>
