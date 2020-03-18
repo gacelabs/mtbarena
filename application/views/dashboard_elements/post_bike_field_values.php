43c315c20ef7039e8259b9a4d18b43d0e324fe4e
unknown <edilitogarcia@gmail.com>
2020-03-18 10:32:38 +0800

    style enhancement

--- application/views/dashboard_elements/post_bike_field_values.php
+++ application/views/dashboard_elements/post_bike_field_values.php
@@ -3,7 +3,7 @@
 ?>
 <div class="box-item" style="margin-bottom: 15px;">
 	<div class="box-item-body-top">
-		<p class="zero-gap color-theme"><b>Post fields</b></p>
+		<p class="zero-gap color-theme"><b>Specifications Template</b></p>
 	</div>
 	
 	<div class="box-item-body" style="padding:0;">
@@ -17,32 +17,32 @@
 						<div class="clearfix form-step-block mainset" id="<?php echo $field['base'];?>">
 							<div class="col-lg-12">
 								<div class="form-group">
-									<label class="zero-gap">Bike part</label>
- 									<h4 style="margin-top: 0"><b class="color-theme"><input type="text" class="form-control" name="fields_data[<?php echo $key;?>][base]" value="<?php echo ucfirst($field['base']);?>" placeholder="Frame, Fork, Saddle, etc..." /></b></h4>
+ 									<h4 style="margin-top: 0"><b class="color-theme"><input type="text" class="form-control" name="fields_data[<?php echo $key;?>][base]" value="<?php echo ucfirst($field['base']);?>" placeholder="Bike part: Frame, Fork, Saddle, etc..." /></b></h4>
 								</div>
 							</div>
 							<?php 
 								$cnt = 0;
 								foreach ($field['values'] as $column => $row): ?>
 								<div class="form-step-body clearfix fieldset">
-									<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 form-step-block-label">
-										<label class="zero-gap">Sort<input type="text" class="form-control" name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][sort]" value="<?php echo ucfirst($row['sort']);?>"></label>
+									<div class="col-lg-1 col-md-1 col-sm-1 col-xs-3 form-step-block-label">
+										<input type="text" class="form-control" name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][sort]" value="<?php echo ucfirst($row['sort']);?>" placeholder="#" />
 									</div>
-									<div class="col-lg-3 col-md-3 col-sm-3 form-step-block-label">
-										<label class="zero-gap">Spec label<input type="text" class="form-control" name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][column]" value="<?php echo ucfirst($row['column']);?>"></label>
-										<div>&nbsp;</div>
-										<label class="zero-gap">Label info</label>
-										<p class="sub-label-text text-left"><input type="text" class="form-control" name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][description]" value="<?php echo $row['description'];?>"></p>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-8 form-step-block-label spec-desc-box">
+										<div class="form-group" style="margin-bottom:5px;">
+											<input type="text" class="form-control" name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][column]" value="<?php echo ucfirst($row['column']);?>" placeholder="Spec label: Brand, Sizes, Types, etc..." />
+										</div>
+										<div class="form-group">
+											<input type="text" class="form-control" name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][description]" value="<?php echo $row['description'];?>" placeholder="Label description." />
+										</div>
 									</div>
 									<div class="col-lg-6 col-md-6 col-sm-6 form-step-block-input">
 										<div class="form-group">
-										<label class="zero-gap">Values</label>
-											<textarea placeholder="Comma separated value. Value1,Value2,Value3..." name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][data]" class="form-control"><?php echo $row['data'];?></textarea>
+											<textarea placeholder="Values: Comma separated value. Value1,Value2,Value3..." name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][data]" class="form-control"><?php echo $row['data'];?></textarea>
 										</div>
 									</div>
 									<div class="col-lg-2 col-md-2 col-sm-2 form-step-block-input">
-										<label class="zero-gap">Multiple</label>
 										<select class="form-control" name="fields_data[<?php echo $key;?>][values][<?php echo $cnt;?>][max]">
+											<option disabled>Multi select?</option>
 											<option value="1"<?php echo $row['max'] == 1 ? ' selected' : '';?>>Yes</option>
 											<option value="0"<?php echo $row['max'] == 0 ? ' selected' : '';?>>No</option>
 										</select>
@@ -58,12 +58,15 @@
 						</div>
 					<?php endforeach ?>
 				<?php endif ?>
-				<div style="padding:10px;">
-					<div class="form-step-footer clearfix add_mainset" data-key="<?php echo $key;?>" style="padding:10px 0;">
-						<span class="btn btn-xs text-info"><i class="fa fa-plus"></i> New Bike part</span>
+				<div class="clearfix">
+					<div class="col-lg-12">
+						<div class="form-step-footer add_mainset" data-key="<?php echo $key;?>" style="padding:10px 0;">
+							<span class="btn btn-xs text-info"><i class="fa fa-plus"></i> New Bike part</span>
+						</div>
+						<hr>
+						<button type="submit" class="btn btn-info">Save</button>
+						<div class="hidden-xs"><br></div>
 					</div>
-					<hr>
-					<button type="submit" class="btn btn-info">Save</button>
 				</div>
 			</form>
 
@@ -71,38 +74,37 @@
 				<div class="clearfix form-step-block mainset">
 					<div class="col-lg-12">
 						<div class="form-group">
-							<label class="zero-gap">Bike part</label>
-							<h4 style="margin-top: 0"><b class="color-theme"><input type="text" class="form-control" name="fields_data[CHANGEKEY][base]" value="" placeholder="Frame, fork, saddle, etc..." /></b></h4>
+							<h4 style="margin-top: 0"><b class="color-theme"><input type="text" class="form-control" name="fields_data[CHANGEKEY][base]" value="" placeholder="Bike part: Frame, fork, saddle, etc..." /></b></h4>
 						</div>
 					</div>
 					<div class="form-step-body clearfix fieldset" id="field_template">
-						<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 form-step-block-label">
-							<label class="zero-gap">Sort<input type="text" class="form-control" name="fields_data[CHANGEKEY][values][CHANGECOL][sort]" value=""></label>
+						<div class="col-lg-1 col-md-1 col-sm-1 col-xs-3 form-step-block-label">
+							<input type="text" class="form-control" name="fields_data[CHANGEKEY][values][CHANGECOL][sort]" value="" placeholder="#">
 						</div>
-						<div class="col-lg-3 col-md-3 col-sm-3 form-step-block-label">
-							<div class="form-group">
-								<label class="zero-gap">Spec label<input type="text" class="form-control" name="fields_data[CHANGEKEY][values][CHANGECOL][column]" value="" placeholder="Brand, Sizes, Types, etc..."></label>
+						<div class="col-lg-3 col-md-3 col-sm-3 col-xs-8 form-step-block-label spec-desc-box">
+							<div class="form-group" style="margin-bottom:5px;">
+								<input type="text" class="form-control" name="fields_data[CHANGEKEY][values][CHANGECOL][column]" value="" placeholder="Spec label: Brand, Sizes, Types, etc...">
 							</div>
 							<div class="form-group">
-								<label class="zero-gap">Label info</label>
-								<p class="sub-label-text text-left"><input type="text" class="form-control" id="" name="fields_data[CHANGEKEY][values][CHANGECOL][description]" value=""></p>
+								<p class="sub-label-text text-left">
+									<input type="text" class="form-control" id="" name="fields_data[CHANGEKEY][values][CHANGECOL][description]" value="" placeholder="Label description.">
+								</p>
 							</div>
 						</div>
 						<div class="col-lg-6 col-md-6 col-sm-6 form-step-block-input">
 							<div class="form-group">
-								<label class="zero-gap">Values</label>
-								<textarea placeholder="Comma separated value. Value1,Value2,Value3..." name="fields_data[CHANGEKEY][values][CHANGECOL][data]" class="form-control"></textarea>
+								<textarea placeholder="Values: Comma separated value. Value1,Value2,Value3..." name="fields_data[CHANGEKEY][values][CHANGECOL][data]" class="form-control"></textarea>
 							</div>
 						</div>
 						<div class="col-lg-2 col-md-2 col-sm-2 form-step-block-input">
-							<label class="zero-gap">Multiple</label>
 							<select class="form-control" name="fields_data[CHANGEKEY][values][CHANGECOL][max]">
+								<option disabled>Multi select?</option>
 								<option value="1">Yes</option>
 								<option value="0">No</option>
 							</select>
 						</div>
 					</div>
-					<div class="add_fieldset" data-key="MAINCHANGEKEY">
+					<div class="add_fieldset" data-key="MAINCHANGEKEY" style="margin-top:15px;">
 						<span class="btn btn-xs text-info"><i class="fa fa-plus"></i> Add a Spec</span>
 					</div>
 					<input type="hidden" name="fields_data[CHANGEKEY][path]" value="assets/data/jsons/">
