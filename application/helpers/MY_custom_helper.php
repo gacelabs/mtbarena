2159dc7b8f5d297b5c3968b19a6b762f1d48a800
Eddie <eddie@ypdigital.com.au>
2020-03-22 01:47:33 +0800

    updates

--- application/helpers/MY_custom_helper.php
+++ application/helpers/MY_custom_helper.php
@@ -759,3 +759,24 @@ function assemble_fields_data($data=FALSE)
 	}
 	return FALSE;
 }
+
+function csv_to_array($filename='', $delimiter=',')
+{
+	if(!file_exists(get_root_path($filename)) OR !is_readable(get_root_path($filename))) {
+		return FALSE;
+	}
+
+	$header = NULL;
+	$data = array();
+	if (($handle = fopen($filename, 'r')) !== FALSE) {
+		while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE) {
+			if(!$header) {
+				$header = $row;
+			} else {
+				$data[] = array_combine($header, $row);
+			}
+		}
+		fclose($handle);
+	}
+	return $data;
+}
